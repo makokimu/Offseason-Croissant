eeade8d3a392943f3217ea1e22976f9fbace175f
Matthew Morley <matthew.morley.ca@gmail.com>
2019-04-14 15:47:48 -0700

    add build files

---
+++ build.gradle.kts
@@ -0,0 +1,85 @@
+import edu.wpi.first.gradlerio.GradleRIOPlugin
+import edu.wpi.first.gradlerio.frc.FRCJavaArtifact
+import edu.wpi.first.gradlerio.frc.RoboRIO
+import edu.wpi.first.toolchain.NativePlatforms
+import jaci.gradle.deploy.artifact.FileTreeArtifact
+import jaci.gradle.deploy.context.DeployContext
+
+plugins {
+    id("edu.wpi.first.GradleRIO") version "2019.4.1"
+    id("org.jetbrains.kotlin.jvm") version "1.3.11"
+    id("idea")
+}
+
+val ROBOT_MAIN_CLASS = "frc.robot.Main"
+
+val roborioTargetName = "roborio"
+
+val kMainRobotClass = "frc.robot.Robot"
+
+deploy {
+    targets {
+        // Add the RoboRIO as a target
+        target(roborioTargetName, RoboRIO::class.java, closureOf<RoboRIO> {
+            team = 5190
+        })
+    }
+    artifacts {
+        // Send the JAR to the RoboRIO
+        artifact("frcJava", FRCJavaArtifact::class.java, closureOf<FRCJavaArtifact> {
+            targets.add(roborioTargetName)
+        })
+    }
+}
+
+repositories {
+    jcenter()
+    maven { setUrl("http://dl.bintray.com/kyonifer/maven") }
+    maven { setUrl("https://jitpack.io") }
+    maven { setUrl("http://dev.imjac.in/maven") }
+    mavenLocal()
+}
+
+dependencies {
+    // Kotlin Standard Library and Coroutines
+    compile(kotlin("stdlib"))
+    compile("org.jetbrains.kotlinx", "kotlinx-coroutines-core", "1.1.0")
+
+    // FalconLibrary
+    compile("org.ghrobotics", "FalconLibrary", "d9bb0f5")
+
+    // WPILib and Vendors
+    wpi.deps.wpilib().forEach { compile(it) }
+    wpi.deps.vendor.java().forEach { compile(it) }
+    wpi.deps.vendor.jni(NativePlatforms.roborio).forEach { nativeZip(it) }
+    wpi.deps.vendor.jni(NativePlatforms.desktop).forEach { nativeDesktopZip(it) }
+
+    // compile("openrio.powerup", "MatchData", "2018.01.07")
+
+    // Gson
+    compile("com.github.salomonbrys.kotson", "kotson", "2.5.0")
+
+    compile("com.github.mcm001:pantrycommon:587caa1eab")
+
+    compile("com.github.Oblarg:Oblog:2.8.1")
+
+
+    // XChart for Simulations and Tests
+    compile("org.knowm.xchart", "xchart", "3.2.2")
+
+    // Unit Testing
+    testCompile("junit", "junit", "4.12")
+}
+
+tasks.jar {
+    doFirst {
+        from(configurations.compile.get().map {
+            if (it.isDirectory) it else zipTree(it)
+        })
+        manifest(GradleRIOPlugin.javaManifest(kMainRobotClass))
+    }
+}
+
+tasks.withType<Wrapper>().configureEach {
+    gradleVersion = "5.0"
+}
