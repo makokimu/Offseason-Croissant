e87c4b4a37c31ddf86757eb66ab4d50d54f505c9
Matt <matthew.morley.ca@gmail.com>
2019-04-14 19:53:59 -0700

    move Ports

--- src/main/kotlin/frc/robot/Ports.kt
+++ src/main/kotlin/frc/robot/Ports.kt
@@ -1,12 +1,16 @@
-package frc.robot.subsystems.drive
+package frc.robot
 
 object Ports {
 
     const val kPCMID = 9
 
     object DrivePorts {
+        val kLeftPorts = listOf(0, 1)
+        val kRightPorts = listOf(2, 3)
         val kShifterPorts = listOf(4, 5)
     }
+
+    object
 
 
 }
