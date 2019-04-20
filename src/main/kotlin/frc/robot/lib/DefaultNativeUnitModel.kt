53a2249f7bc6c08604635466f5c0577a9dfb4a93
Matt <matthew.morley.ca@gmail.com>
2019-04-20 13:54:34 -0500

    Delete DefaultNativeUnitModel.kt

--- src/main/kotlin/frc/robot/lib/DefaultNativeUnitModel.kt
+++
@@ -1,9 +0,0 @@
-package frc.robot.lib
-
-import org.ghrobotics.lib.mathematics.units.nativeunits.NativeUnit
-import org.ghrobotics.lib.mathematics.units.nativeunits.NativeUnitModel
-
-object DefaultNativeUnitModel : NativeUnitModel<NativeUnit>(NativeUnit.kZero) {
-    override fun fromNativeUnitPosition(nativeUnits: Double): Double = nativeUnits
-    override fun toNativeUnitPosition(modelledUnit: Double): Double = modelledUnit
-}
