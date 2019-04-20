ac70b1a8573d371d9d038f8a070200b108f5cbcd
Matt <matthew.morley.ca@gmail.com>
2019-04-20 09:30:13 -0500

    Add example drive command to robot container

--- src/main/kotlin/frc/robot/RobotContainer.kt
+++ src/main/kotlin/frc/robot/RobotContainer.kt
@@ -1,6 +1,8 @@
 package frc.robot
 
+import edu.wpi.first.wpilibj.GenericHID
 import edu.wpi.first.wpilibj.XboxController
+import frc.robot.lib.AButtonButton
 import frc.robot.subsystems.drive.Drive
 import frc.robot.subsystems.superstructure.SuperStructure
 
@@ -14,11 +16,12 @@ class RobotContainer {
 
     val secondary : XboxController = XboxController(1)
 
-
     fun bindButtons() {
 
-
-
+        drive.defaultCommand = drive.curvatureDriveCommand(
+                {primary.getY(GenericHID.Hand.kLeft)},
+                {primary.getX(GenericHID.Hand.kRight) * -1},
+                {primary.getBumper(GenericHID.Hand.kLeft) || primary.getBumper(GenericHID.Hand.kRight)}
+        )
     }
-
 }
