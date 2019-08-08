2f935bcaed98ee0c2723f0805187770623aa06bc
Matt <matthew.morley.ca@gmail.com>
2019-08-08 16:39:20 -0500

    Add method to convert trajectory -> Json and json -> trajectory

---
+++ src/test/kotlin/frc/robot/subsystems/superstructure/GsonTest.kt
@@ -0,0 +1,31 @@
+package frc.robot.subsystems.superstructure
+
+import com.github.salomonbrys.kotson.fromJson
+import com.google.gson.JsonElement
+import com.google.gson.JsonObject
+import org.ghrobotics.lib.mathematics.twodim.trajectory.DefaultTrajectoryGenerator
+import org.junit.Test
+import org.team5940.pantry.lib.jsonToTrajectory
+import org.team5940.pantry.lib.kGson
+import org.team5940.pantry.lib.toJson
+
+class GsonTest {
+
+    val test: Double
+        get() = Math.random()
+
+    fun getTest(yes: Double) = test
+
+    @Test
+    fun testGson() {
+
+        val trajectory = DefaultTrajectoryGenerator.baseline
+
+        val jsonedTrajectory = trajectory.toJson()
+
+        val unJsoned = jsonToTrajectory(jsonedTrajectory)
+
+        println(unJsoned)
+    }
+
+}
