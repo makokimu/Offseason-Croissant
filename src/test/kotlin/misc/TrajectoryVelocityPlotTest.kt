4a52f033757f9c6ab8a3f62f57e8918ce8f4ffe2
Matt <matthew.morley.ca@gmail.com>
2019-10-09 23:10:52 -0700

    Add velocity and voltage visualizer

--- src/test/kotlin/misc/TrajectoryVelocityPlotTest.kt
+++ src/test/kotlin/misc/TrajectoryVelocityPlotTest.kt
@@ -1,5 +1,7 @@
 package misc
 
+import com.team254.lib.physics.DifferentialDrive
+import frc.robot.Constants
 import org.ghrobotics.lib.mathematics.twodim.geometry.Pose2dWithCurvature
 import org.ghrobotics.lib.mathematics.twodim.trajectory.types.TimedTrajectory
 import org.ghrobotics.lib.mathematics.units.derived.feetPerSecond
@@ -12,22 +14,30 @@ import java.awt.Color
 import java.awt.Font
 import java.text.DecimalFormat
 import frc.robot.auto.paths.TrajectoryFactory
+import javax.transaction.xa.Xid
+import kotlin.math.absoluteValue
+import kotlin.math.max
+import kotlin.math.min
 
 class TrajectoryVelocityPlotTest {
 
     @Test
     fun test() {
-        displayTrajectory(TrajectoryFactory.sideStartReversedToRocketFPrepare)
+//        val (x1, y1) = trajectToData(TrajectoryFactory.sideStartReversedToRocketFPrepare)
+        val (x1, y1, z1) = trajectToData(TrajectoryFactory.rocketFPrepareToLoadingStation)
+
+        displayTrajectory(Triple(x1, y1, z1))
     }
 
-    fun displayTrajectory(trajectory: TimedTrajectory<Pose2dWithCurvature>) {
+
+    private fun trajectToData(trajectory: TimedTrajectory<Pose2dWithCurvature>): Triple<ArrayList<Double>, ArrayList<Double>, ArrayList<Double>> {
 
         var currentTime = 0.second
         val deltaTime = 20.milli.second
 
         val xList = arrayListOf<Double>()
         val yList = arrayListOf<Double>()
-
+        val zList = arrayListOf<Double>()
         val iterator = trajectory.iterator()
         while (!iterator.isDone) {
             currentTime += deltaTime
@@ -35,9 +45,26 @@ class TrajectoryVelocityPlotTest {
             val velocity = sample.velocity
             xList += currentTime.second
             yList += velocity.feetPerSecond
+
+            val vd = sample.velocity.value
+            val wd = vd * sample.state.curvature
+
+            val voltage = Constants.DriveConstants.kHighGearDifferentialDrive.getVoltagesFromkV(
+                    Constants.DriveConstants.kHighGearDifferentialDrive.solveInverseKinematics(DifferentialDrive.ChassisState(vd, wd))
+            ).let { max(it.left, it.right)}
+            zList += voltage
         }
 
-        val fm = DecimalFormat("#.###").format(trajectory.lastInterpolant.second)
+        return Triple(xList, yList, zList)
+    }
+
+    private fun displayTrajectory(data: Triple<ArrayList<Double>, ArrayList<Double>, ArrayList<Double>>) {
+
+        val xList = data.first
+        val yList = data.second
+        val zList = data.third
+
+        val fm = DecimalFormat("#.###").format(xList.max()!!)
 
         val chart = XYChartBuilder().width(1800).height(1520).title("$fm seconds.")
                 .xAxisTitle("X").yAxisTitle("Y").build()
@@ -48,10 +75,10 @@ class TrajectoryVelocityPlotTest {
         chart.styler.chartTitleFont = Font("Kanit", 1, 40)
         chart.styler.chartTitlePadding = 15
 
-        chart.styler.xAxisMin = 1.0
-        chart.styler.xAxisMax = 26.0
-        chart.styler.yAxisMin = 1.0
-        chart.styler.yAxisMax = 26.0
+        chart.styler.xAxisMin = xList.min()!!
+        chart.styler.xAxisMax = xList.max()!!
+        chart.styler.yAxisMin = min(yList.min()!!, zList.min()!!)
+        chart.styler.yAxisMax = max(yList.max()!!, zList.max()!!)
 
         chart.styler.chartFontColor = Color.WHITE
         chart.styler.axisTickLabelsColor = Color.WHITE
@@ -66,6 +93,7 @@ class TrajectoryVelocityPlotTest {
         chart.styler.plotBackgroundColor = Color.DARK_GRAY
 
         chart.addSeries("Robot velocity", xList.toDoubleArray(), yList.toDoubleArray())
+        chart.addSeries("Voltage", xList.toDoubleArray(), zList.toDoubleArray())
 
         SwingWrapper(chart).displayChart()
         Thread.sleep(1000000)
